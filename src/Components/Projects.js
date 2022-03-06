b224c2c91cced1925677ef242764b1b80af9e1fa
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-03-05 22:09:31 -0800

    project links added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,6 +6,26 @@ const About = () => {
     return <>
         <div id="projectsAnchor" className="projectsContainer">
             Hello Projects
+            <div className="pokedex projectCard"> 
+                <h3> Pokedex </h3>
+                <a className="projectLink" href="https://practical-ardinghelli-a24fb5.netlify.app/" target="_blank">Deploy</a>
+                <a className="projectLink" href="https://github.com/Felix-Cadiz/Pokemon_Generator" target="_blank">Repository</a>
+            </div>
+            <div className="porto3000 projectCard"> 
+                <h3> Porto 3000 </h3>
+                <a className="projectLink" href="http://porto3000.herokuapp.com/" target="_blank">Deploy</a>
+                <a className="projectLink" href="https://github.com/hoguer/porto3000" target="_blank">Repository</a>
+            </div>
+            <div className="fitnessTracker projectCard"> 
+                <h3> Fitness Tracker</h3>
+                <a className="projectLink" href="https://admiring-bhaskara-04c615.netlify.app/" target="_blank">Deploy</a>
+                <a className="projectLink" href="https://github.com/Felix-Cadiz/FitnessTrackr-FrontEnd" target="_blank">Repository</a>
+            </div>
+            <div className="portfolio projectCard"> 
+                <h3> Personal Portfolio </h3>
+                <a className="projectLink" href="google.com" target="_blank">Deploy</a>
+                <a className="projectLink" href="google.com" target="_blank">Repository</a>
+            </div>
         </div>
     </>
 }
