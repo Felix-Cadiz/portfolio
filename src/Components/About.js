0dc0b3cf21cbcd3ac763addca93bd25908d6a36c
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-03-09 20:57:16 -0800

    projects hover effect functional

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,11 +6,15 @@ const About = () => {
     return <>
         <div id="aboutAnchor" className="aboutContainer container">
             <h1> ABOUT ME </h1>
-            <h3> Music Education </h3>
-            <h3> Transition into Tech </h3>
             <div className="twoStories">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium eros ligula, sed vulputate nulla ullamcorper sed. Aenean elementum sit amet felis at finibus. Vestibulum tincidunt urna non orci lacinia sollicitudin nec gravida risus. Sed nunc risus, mattis vitae elit non, vulputate sollicitudin elit. Maecenas vitae varius arcu. Nullam non turpis sodales, efficitur sem ac, gravida tortor. Sed tincidunt.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium eros ligula, sed vulputate nulla ullamcorper sed. Aenean elementum sit amet felis at finibus. Vestibulum tincidunt urna non orci lacinia sollicitudin nec gravida risus. Sed nunc risus, mattis vitae elit non, vulputate sollicitudin elit. Maecenas vitae varius arcu. Nullam non turpis sodales, efficitur sem ac, gravida tortor. Sed tincidunt.</p>
+                <div className="music">
+                    <h3> Music Education </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium eros ligula, sed vulputate nulla ullamcorper sed. Aenean elementum sit amet felis at finibus. Vestibulum tincidunt urna non orci lacinia sollicitudin nec gravida risus. Sed nunc risus, mattis vitae elit non, vulputate sollicitudin elit. Maecenas vitae varius arcu. Nullam non turpis sodales, efficitur sem ac, gravida tortor. Sed tincidunt.</p>
+                </div>
+                <div className="tech">
+                    <h3> Transition into Tech </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium eros ligula, sed vulputate nulla ullamcorper sed. Aenean elementum sit amet felis at finibus. Vestibulum tincidunt urna non orci lacinia sollicitudin nec gravida risus. Sed nunc risus, mattis vitae elit non, vulputate sollicitudin elit. Maecenas vitae varius arcu. Nullam non turpis sodales, efficitur sem ac, gravida tortor. Sed tincidunt.</p>
+                </div>
             </div>
         </div>
     </>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,28 +4,48 @@ import "./Projects.css"
 const About = () => {
 
     return <>
-        <div id="projectsAnchor" className="projectsContainer container">
-            <div className="projectCard"> 
-                <div className="pokedex"></div>
-                {/* <h3> Pokedex </h3>
-                    <a className="projectLink" href="https://practical-ardinghelli-a24fb5.netlify.app/" target="_blank">Deploy</a>
-                    <a className="projectLink" href="https://github.com/Felix-Cadiz/Pokemon_Generator" target="_blank">Repository</a>
-                <div> Short description on hover </div> */}
+        <div className="projectsContainer container">
+        <h1 id="projectsAnchor"> Recent Projects </h1>
+        {/* <div className="projectsContainer"></div> */}
+            <div className="projectCard"> Pokedex
+                <div className="pokedex">
+                    <div className="projectDescription">
+                        <h3> Pokedex </h3>
+                        <a className="projectLink" href="https://practical-ardinghelli-a24fb5.netlify.app/" target="_blank">Deploy</a>
+                        <a className="projectLink" href="https://github.com/Felix-Cadiz/Pokemon_Generator" target="_blank">Repository</a>
+                        <p> Learn about the world of Pokemon through this React app. </p>
+                    </div>
+                </div>
             </div>
-            <div className="porto3000 projectCard"> 
-                <h3> Porto 3000 </h3>
-                    <a className="projectLink" href="http://porto3000.herokuapp.com/" target="_blank">Deploy</a>
-                    <a className="projectLink" href="https://github.com/hoguer/porto3000" target="_blank">Repository</a>
+            <div className="projectCard">  Porto 3000
+                <div className="porto3000">
+                    <div className="projectDescription">
+                        <h3> Porto 3000 </h3>
+                        <a className="projectLink" href="http://porto3000.herokuapp.com/" target="_blank">Deploy</a>
+                        <a className="projectLink" href="https://github.com/hoguer/porto3000" target="_blank">Repository</a>
+                        <div> Short description on hover </div>
+                    </div>
+                </div>
             </div>
-            <div className="fitnessTracker projectCard"> 
-                <h3> Fitness Tracker</h3>
-                    <a className="projectLink" href="https://admiring-bhaskara-04c615.netlify.app/" target="_blank">Deploy</a>
-                    <a className="projectLink" href="https://github.com/Felix-Cadiz/FitnessTrackr-FrontEnd" target="_blank">Repository</a>
+            <div className="projectCard"> Fitness Tracker
+                <div className="fitnessTracker">
+                    <div className="projectDescription">
+                        <h3> Fitness Tracker</h3>
+                        <a className="projectLink" href="https://admiring-bhaskara-04c615.netlify.app/" target="_blank">Deploy</a>
+                        <a className="projectLink" href="https://github.com/Felix-Cadiz/FitnessTrackr-FrontEnd" target="_blank">Repository</a>
+                        <div className="projectText"> Short description on hover</div>
+                    </div>
+                </div>
             </div>
-            <div className="portfolio projectCard"> 
-                <h3> Personal Portfolio </h3>
-                    <a className="projectLink" href="google.com" target="_blank">Deploy</a>
-                    <a className="projectLink" href="google.com" target="_blank">Repository</a>
+            <div className="portfolio projectCard"> Portfolio
+                <div className="portfolio">
+                    <div className="projectDescription">
+                        <h3> Personal Portfolio </h3>
+                        <a className="projectLink" href="google.com" target="_blank">Deploy</a>
+                        <a className="projectLink" href="google.com" target="_blank">Repository</a>
+                        <div> Short description on hover </div>
+                    </div>
+                </div>
             </div>
         </div>
     </>
