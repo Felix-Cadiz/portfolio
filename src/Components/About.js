48c6423c9faac392553ed7dc77c55832eac40eb5
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2022-03-26 05:31:21 -0700

    Headshot and Python update

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,17 +7,17 @@ const About = () => {
         <div id="aboutAnchor" className="aboutContainer container">
             <h1> ABOUT ME </h1>
             <div className="story">
-                    <p> Hi, I'm Felix, a full stack web developer with a background in teaching music 
+                    <p className="animate__animated animate__fadeInLeft animate__delay-3s"> Hi, I'm Felix, a full stack web developer with a background in teaching music 
                         in a public school setting. A graduate of FullStack Academy, I mostly program 
                         in Javascript; using backend frameworks like <b>Node.js</b> and <b>Express</b>, 
                         and frontend frameworks like <b>React</b>. 
                      </p>
-                    <p> I enjoy working with the client side, creating accessible, inviting, and memorable 
+                    <p className="animate__animated animate__fadeInLeft animate__delay-4s"> I enjoy working with the client side, creating accessible, inviting, and memorable 
                         experiences to all users. Manipulating <b>CSS</b>, creating custom animations, and 
                         pulling data from APIs. As a former teacher in the performing arts, I understand 
                         that it is not only about the content, but the manner in which it is presented. 
                     </p>
-                    <p> Despite only starting to code in Mid-2021, building digital projects has become 
+                    <p className="animate__animated animate__fadeInLeft animate__delay-5s"> Despite only starting to code in Mid-2021, building digital projects has become 
                         a great hobby of mine. Aside from coding, I enjoy creating music with a variety 
                         of instruments, yoyoing, experiencing musical theatre, and catching Pokemon in the 
                         newest region. 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,6 +6,39 @@ const Projects = () => {
     return <>
     <h1 id="projectsAnchor"> Recent Projects </h1>
         <div className="projectsContainer container">
+            <div className="pythonChallenge projectCard">
+                <h3> 100 days of Python </h3>
+                <p> March 2022 </p>
+                <img className="portfolio" src="images/python.jpeg" alt="python" />
+                <div className="projectDescription">
+                    <a className="button" href="https://github.com/Felix-Cadiz/100_days_of_Python" target="_blank">Repository</a>
+                    <div className="projectInformation">
+                        <ul className="technologies"> Technologies:
+                            <li> Python </li>
+                            <li> VS Code </li>
+                            <li> replit.com </li>
+                        </ul>
+                        <p className="projectAbout"> A collection of various projects showcasing my acquisition of a new language. <br/><br/> *Work in progress</p>
+                    </div>
+                </div>
+            </div>
+            <div className="portfolio projectCard">
+                <h3> Personal Portfolio </h3>
+                <p> Febuary 2022 - March 2022</p>
+                <img className="portfolio" src="images/Portfolio.png" alt="portofio" />
+                <div className="projectDescription">
+                    <a className="button" href="google.com" target="_blank">Deploy</a>
+                    <a className="button" href="google.com" target="_blank">Repository</a>
+                    <div className="projectInformation">
+                        <ul className="technologies"> Technologies:
+                            <li> Javascript </li>
+                            <li> React </li>
+                            <li> CSS </li>
+                        </ul>
+                        <p className="projectAbout"> Responsive personal website.</p>
+                    </div>
+                </div>
+            </div>
             <div className="projectCard">
                 <h3> Pokedex </h3>
                 <p> February 2022 </p> 
@@ -59,23 +92,6 @@ const Projects = () => {
                         </ul>
                         <p className="projectAbout"> Application that allows users to create, share, edit, 
                         and assemble their favorite fitness routines and activites.</p>
-                    </div>
-                </div>
-            </div>
-            <div className="portfolio projectCard">
-                <h3> Personal Portfolio </h3>
-                <p> Febuary 2022 </p>
-                <img className="portfolio" src="images/Portfolio.png" alt="portofio" />
-                <div className="projectDescription">
-                    <a className="button" href="google.com" target="_blank">Deploy</a>
-                    <a className="button" href="google.com" target="_blank">Repository</a>
-                    <div className="projectInformation">
-                        <ul className="technologies"> Technologies:
-                            <li> Javascript </li>
-                            <li> React </li>
-                            <li> CSS </li>
-                        </ul>
-                        <p className="projectAbout"> Responsive personal website.</p>
                     </div>
                 </div>
             </div>
